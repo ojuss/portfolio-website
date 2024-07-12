395402336bd2e06bde9ebec4be2539ed4d5e0aa8
Ojus <guptaojas75@gmail.com>
2024-07-12 13:55:07 +0530

    fix vercel error

--- pages/misc.tsx
+++ pages/misc.tsx
@@ -40,7 +40,7 @@ const Misc = () => {
           <h1 className={"text-4xl font-semibold"}>./guitar</h1>
           <div className="mt-4 pb-12 space-y-4">
            <p className="text-lg">
-           <span className="text-pink-400">Six strings with fretboard:</span> I love playing and composing music on acoustic guitar with my friend. Right now, I'm learning fingerstyle, which is a way of playing where you use your fingers instead of a pick. I'm trying to learn the song <br/> <span className="text-blue-300">"No Idea"</span> by Don Toliver.
+           <span className="text-pink-400">6 strings</span> I love playing and composing music on acoustic guitar with my friend. Right now, I'm learning fingerstyle, which is a way of playing where you use your fingers instead of a pick. I'm trying to learn the song <span className="text-blue-300">"No Idea"</span> by Don Toliver.
            </p>
             <p>
               Currently I haven't posted my songs anywhere yet but thinking to do that pretty soon!
